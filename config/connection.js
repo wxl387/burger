df3e36d83130246fc0c2e26a32730942932b786e
Wenbiao Li <lixuyao.tony@gmail.com>
2017-10-23 17:06:35 -0400

    add JawsDB connection"

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
   
   user: "root",
   password: "root",
   database: "burgers_db"
-});
+  });
+};
 
 connection.connect(function(err) {
 	if (err) {
